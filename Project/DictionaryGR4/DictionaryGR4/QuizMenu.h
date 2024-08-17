6e70fc9f75212f817f3daa312ae8f30ca8a5753a
Cao Thanh Hieu <nguoidaulungsiucapvjppro@gmail.com>
2024-08-17 12:24:02 +0700

    modify quizmenu

--- Project/DictionaryGR4/DictionaryGR4/QuizMenu.h
+++ Project/DictionaryGR4/DictionaryGR4/QuizMenu.h
@@ -14,7 +14,16 @@ public:
 	QuizMenu(wxWindow* parent);
 
 private:
+	wxStaticText* game,* datachose,* numberques;
+	wxButton* GuessWord, * GuessDef, *EngEng, *EngVie, *VieEng;
+	wxBitmapButton* increase, * decrease; 
 
+	/*==============GAME_VARIABLE==============*/
+	int current_question = 0, score = 0, numquest = 30;
+	int gametype = 0; //0: Guess Def; 1: GuessWord
+	int dataset = 0; //0: Eng-Eng, 1: Eng-Viet, 2: Viet-Eng
+	void DefaultSetting();
+	/*=========================================*/
 
 };
 #endif
