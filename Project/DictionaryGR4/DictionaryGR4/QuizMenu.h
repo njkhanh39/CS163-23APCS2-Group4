1399ec3cea21714e7fcd5c0a7979e60067dc6454
Cao Thanh Hieu <nguoidaulungsiucapvjppro@gmail.com>
2024-08-22 14:55:14 +0700

    A bit modify in QuizMenu

--- Project/DictionaryGR4/DictionaryGR4/QuizMenu.h
+++ Project/DictionaryGR4/DictionaryGR4/QuizMenu.h
@@ -8,22 +8,37 @@
 #include <cstring>
 #include <algorithm>
 
-class QuizMenu : public wxPanel {
+class QuizMenu : public wxSimplebook {
 public:
 
 	QuizMenu(wxWindow* parent);
 
 private:
-	wxStaticText* game,* datachose,* numberques;
-	wxButton* GuessWord, * GuessDef, *EngEng, *EngVie, *VieEng;
-	wxBitmapButton* increase, * decrease; 
-
+	  
 	/*==============GAME_VARIABLE==============*/
 	int current_question = 0, score = 0, numquest = 30;
 	int gametype = 0; //0: Guess Def; 1: GuessWord
 	int dataset = 0; //0: Eng-Eng, 1: Eng-Viet, 2: Viet-Eng
+	bool is_endless = false;
 	void DefaultSetting();
 	/*=========================================*/
 
+	wxPanel* gamevar;
+	wxStaticText* game, * datachose, * numberques;
+	wxButton* GuessWord, * GuessDef, * EngEng, * EngVie, * VieEng, *start, *endlessmode;
+	wxBitmapButton* increase, * decrease;
+
+	/*=================QUESTION================*/
+	wxPanel* question;
+	wxStaticBox* chosen_box;
+	wxStaticText* chosen_quest;
+	wxButton* options[4], exit;
+	/*=========================================*/
+
+	/*=================SCORE===================*/
+	wxPanel* score_announce;
+	wxStaticText* announce;
+	wxButton* restart;
+	/*=========================================*/
 };
 #endif
