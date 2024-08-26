90060c3c59b84bab54f2cbc9ed8d617c521b0b05
Khanh <nguyengiakhanh7979@gmail.com>
2024-08-26 13:04:08 +0700

    minor fix

--- Project/DictionaryGR4/DictionaryGR4/HistoryMenu.cpp
+++ Project/DictionaryGR4/DictionaryGR4/HistoryMenu.cpp
@@ -8,7 +8,7 @@ HistoryMenu::~HistoryMenu() {
 
 }
 
-HistoryMenu::HistoryMenu(wxWindow* parent, Dictionary*& dict) {
+HistoryMenu::HistoryMenu(wxWindow* parent, Dictionary*& dict): wxPanel(parent, wxID_ANY, wxDefaultPosition, wxSize(1280, 720), wxBORDER_NONE) {
 	auto purple = wxColour(101, 86, 142), red = wxColour(184, 89, 89), green = wxColour(11, 199, 189), white = wxColour(255, 255, 255), black = wxColour(34, 36, 40);
 
 	//font
--- Project/DictionaryGR4/DictionaryGR4/MainFrame.cpp
+++ Project/DictionaryGR4/DictionaryGR4/MainFrame.cpp
@@ -39,7 +39,7 @@ MainFrame::MainFrame(Dictionary*& dict, const wxString& Title, wxSize FrameSize)
     simpleBook->AddPage(quizMenu, "Quiz Menu", true); //selection = 1
     simpleBook->AddPage(searchMenu, "Search Menu", true);//selection = 2
 	//simpleBook->AddPage(mainMenu, "Main Menu", true); //selection = 2
-    //simpleBook->AddPage(histMenu, "History Menu", true);
+    simpleBook->AddPage(histMenu, "History Menu", true);
 
     simpleBook->SetSelection(2);
    
